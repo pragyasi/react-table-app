843464ad0e22a21c0d0f400dcee44bf14e9dcd92
Pragya Singh <pragyasingh@Pragyas-MacBook-Pro.local>
2020-11-20 13:56:48 +0530

    revised

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import './App.css';
-import {messages} from './data.json';
-import {removeDuplicates , currentPage } from  './removeDuplicates';
-import {useState , useEffect} from 'react';
-import {sortUser} from './updateData';
-import {getHeader , getTableBody} from './tableRender';
-const headerNames = ['Sender' , 'Sent At' , 'Action'];
- 
-const userData = removeDuplicates(messages);
-const perPageUserData = currentPage(userData,'first');
+import './App.css'
+import { messages } from './data.json'
+import { removeDuplicates, currentPage } from './utility'
+import { useState, useEffect } from 'react'
+import { sortUser } from './updateData'
+import { getHeader, getTableBody } from './tableRender'
+const headerNames = ['Sender', 'Sent At', 'Action']
 
-function App() {
- let [userStore , userUpdateStore] = useState(perPageUserData());
+const userData = removeDuplicates(messages)
+const perPageUserData = currentPage(userData, 'first')
+
+function App () {
+  let [userStore, userUpdateStore] = useState(perPageUserData())
   return (
     <div>
-      <table className ='userTable'>
-        <thead id='header'>{getHeader(userStore , userUpdateStore)}</thead>
-        <tbody>{getTableBody(userStore  , userUpdateStore , userData)}</tbody>
-        </table>
+      <table className='userTable'>
+        <thead id='header'>{getHeader(userStore, userUpdateStore)}</thead>
+        <tbody>{getTableBody(userStore, userUpdateStore, userData)}</tbody>
+      </table>
       <footer className='paging'>
-        <button onClick ={()=>{
-          const prev = perPageUserData( 'prev')
-          userUpdateStore(prev)}}>Previous</button>
-        <button onClick ={()=>{
-          const next = perPageUserData('next')
-          userUpdateStore(next)
-          }}>
-          Next</button>
-        </footer>
+        <button
+          onClick={() => {
+            const prev = perPageUserData('prev')
+            userUpdateStore(prev)
+          }}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => {
+            const next = perPageUserData('next')
+            userUpdateStore(next)
+          }}
+        >
+          Next
+        </button>
+      </footer>
     </div>
   )
 }
-export default App;
+export default App
--- src/updateData.js
+++ src/updateData.js
@@ -1,17 +1,15 @@
-
-export function removeUser(updatedData,data , userData){
-  userData.forEach((curr , index)=>{
-    if (JSON.stringify(data)=== JSON.stringify(curr)){
-      userData.splice(index , 1)
+export function removeUser (updatedData, data, userData) {
+  userData.forEach((curr, index) => {
+    if (JSON.stringify(data) === JSON.stringify(curr)) {
+      userData.splice(index, 1)
     }
   })
-  return updatedData.filter((curr)=>{
-    return  JSON.stringify(data) !== JSON.stringify(curr)
+  return updatedData.filter(curr => {
+    return JSON.stringify(data) !== JSON.stringify(curr)
   })
 }
-export function sortUser(users){
-  return users.slice().sort((a , b)=>{
-    return a.senderUuid - b.senderUuid;
+export function sortUser (users) {
+  return users.slice().sort((a, b) => {
+    return a.senderUuid - b.senderUuid
   })
-
 }
